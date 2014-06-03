ca65459f94c6ee8dd8bdc2747270536c67eaaec0
agori <agori@localhost.localdomain>
2014-06-03 22:45:15 +0200

    license headers added where they were missing

--- src/test/java/info/novatec/beantest/demo/ejb/MyEJBServiceWithEntityManagerSetter.java
+++ src/test/java/info/novatec/beantest/demo/ejb/MyEJBServiceWithEntityManagerSetter.java
@@ -1,3 +1,18 @@
+/*
+ * Bean Testing.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package info.novatec.beantest.demo.ejb;
 
 import javax.ejb.Stateless;
@@ -7,15 +22,15 @@ import javax.persistence.PersistenceContext;
 @Stateless
 public class MyEJBServiceWithEntityManagerSetter {
 	
-	private EntityManager em;
+    private EntityManager em;
 	
-	@PersistenceContext(unitName = "db2")
-	public void setEm(EntityManager em) {
-		this.em = em;
-	}
+    @PersistenceContext(unitName = "db2")
+    public void setEm(EntityManager em) {
+	this.em = em;
+    }
 	
-	public EntityManager getEm() {
-		return em;
-	}
+    public EntityManager getEm() {
+	return em;
+    }
 
 }
